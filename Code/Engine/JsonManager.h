931ba2299e181f483946820ff56e24ea7f8444c5
kiwon132445 <kiwon132445@gmail.com>
2022-10-28 00:52:21 -0700

    re-added njsonarchives and jsonmanager files

---
+++ Code/Engine/JsonManager.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <stdlib.h>
+#include <string>
+#include <nlohmann/json.hpp>
+#include "NJSONArchives.h"
+#include "Engine/ECS/Registry.h"
+
+using json = nlohmann::json;
+namespace sad
+{
+	class JsonManager
+	{
+	public:
+		JsonManager();
+
+		static json GetJson(std::string path);
+
+		/**
+		 * @brief 
+		 * Usage GetJsonContent<expectedDataType>(Json, key value)
+		 * @sample
+		 * std::string jsonContent = sad::JsonManager::GetJsonContent<json>(sad::JsonManager::GetJson(jsonPath), "entities").dump();
+		 * @tparam T 
+		 * json, int, or string
+		 * @param Json 
+		 * @param key 
+		 * @return 
+		*/
+		template <typename T>
+		inline static T GetJsonContent(const json& Json, std::string key)
+		{
+			return Json[key];
+		};
+		void static ImportLevel();
+		void static ExportLevel();
+	};
+}

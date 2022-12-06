f117c549bc3ceb19f97537bd94c607d3ceebdaeb
Mohammed Bajaman <mbajaman@my.bcit.ca>
2022-12-05 23:04:13 -0800

    Basic cel shading based on diffuse value

--- Data/Shaders/Model.glsl
+++ Data/Shaders/Model.glsl
@@ -13,6 +13,8 @@ uniform mat3 u_NormalMatrix;
 uniform mat4 u_ModelViewMatrix;
 uniform mat4 u_MvpMatrix;
 
+varying float intensity;
+
 void main()
 {
     v_TexCoord = in_TexCoord;
@@ -36,40 +38,82 @@ struct Model {
     vec4 Emissive;
 };
 
-out vec4 out_FragColor;
+uniform sampler2D u_Texture;
+uniform vec3 u_LightPosition;
+uniform vec3 u_ViewPosition;
+uniform Model u_Model;
 
 in vec2 v_TexCoord;
 in vec3 v_Normals;
 in vec4 v_FragPos;
 
-uniform sampler2D u_Texture;
-
-uniform vec3 u_LightPosition;
-uniform vec3 u_ViewPosition;
-uniform Model u_Model;
+out vec4 out_FragColor;
 
 void main()
 {
+//    float shininess = 2.0f;
+   
+//
+//    float znear = 1.0;
+//    float zfar = 50000.0;
+//    float depthm = texture2D(u_Texture, v_TexCoord.xy).r;
+//
+//    float lineAmp = mix( 0.001, 0.0, clamp( (500.0 / (zfar + znear - ( 2.0 * depthm - 1.0 ) * (zfar - znear) )/2.0), 0.0, 1.0 ) );// make the lines thicker at close range
+//
+//    float depthn = texture2D(u_Texture, v_TexCoord.xy + vec2( (0.002 + lineAmp)*0.625 , 0.0) ).r;
+//    depthn = depthn / depthm;
+//
+//    float depths = texture2D(u_Texture, v_TexCoord.xy - vec2( (0.002 + lineAmp)*0.625 , 0.0) ).r;
+//    depths = depths / depthm;
+//
+//    float depthw = texture2D(u_Texture, v_TexCoord.xy + vec2(0.0 , 0.002 + lineAmp) ).r;
+//    depthw = depthw / depthm;
+//
+//    float depthe = texture2D(u_Texture, v_TexCoord.xy - vec2(0.0 , 0.002 + lineAmp) ).r;
+//    depthe = depthe / depthm;
+//
+//    float Contour = -4.0 + depthn + depths + depthw + depthe;
+//
+//    float lineAmp2 = 100.0 * clamp( depthm - 0.99, 0.0, 1.0);
+//    lineAmp2 = lineAmp2 * lineAmp2;
+//    Contour = (512.0 + lineAmp2 * 204800.0 ) * Contour;
+//
+//    if(Contour > 0.15){
+//        Contour = (0.15 - Contour) / 1.5 + 0.5;
+//    } else
+//        Contour = 1.0;
+
     // Variables that could be uniforms
     float shininess = 2.0f;
 
     // Ambient
-    float ambientStrength = 12.0f;
-    vec4 ambient = u_Model.Ambient * texture(u_Texture, v_TexCoord) * ambientStrength;
+    float ambientStrength = 3.0f;
+    vec4 ambient = u_Model.Ambient * texture(u_Texture, v_TexCoord);
 
     // Diffuse
     float diffuseStrength = 3.0f;
     vec3 norm = normalize(v_Normals);
-    vec3 lightDir = normalize(u_LightPosition - vec3(v_FragPos));
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec4 diffuse = u_Model.Diffuse * diff * diffuseStrength;
+    vec3 lightDir = normalize(u_ViewPosition);
+    float diff = dot(norm, lightDir);
+    vec4 diffuse = u_Model.Diffuse * diffuseStrength * diff;
+
+    // Basic cel-shading portion
+    if (diff > 0.95)
+        diffuse = diffuse;
+    else if (diff > 0.5)
+        diffuse = diffuse - vec4(0.4, 0.2, 0.2, 1.0);
+    else if (diff > 0.25)
+        diffuse = diffuse - vec4(0.6, 0.3, 0.3, 1.0);
+    else
+        diffuse = diffuse - vec4(0.8, 0.4, 0.4, 1.0);
+        
 
     // Specular
     float specularStrength = 1.5f;
     vec3 viewDir = normalize(u_ViewPosition - vec3(v_FragPos));
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-    vec4 specular = u_Model.Specular * spec * specularStrength;
+    vec4 specular = u_Model.Specular  * spec * specularStrength;
     
     if (spec < 0.0f) 
     {
@@ -77,5 +121,5 @@ void main()
     }
 
     vec3 result = vec3(ambient) + vec3(diffuse);
-    out_FragColor = vec4(result, 1.0) * vec4(vec3(specular), 1.0);
+    out_FragColor = vec4(result, 1.0);
 }
